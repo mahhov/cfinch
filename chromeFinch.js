cf7af859d6d892b05132e9d97ea3b15a158d9c36
manuk <mahhov1@gmail.com>
2020-05-07 17:14:44 -0700

    first comit

---
+++ chromeFinch.js
@@ -0,0 +1,98 @@
+#!/usr/bin/env node
+
+const childProcess = require('child_process');
+const ArgsListParser = require('args-list-parser');
+
+const CHROME_DIR = '~/workspace/chromium/src';
+const BUILD_COMMAND = '~/workspace/depot_tools/ninja -C out/Default -j 1000 chrome';
+const BUILD_OUTPUT_PIPE = '/usr/local/google/home/manukh/personal/colorChrome/translatePaths.js';
+const CHROME_COMMAND = 'out/Default/chrome';
+const CHROME_OUTPUT_PIPE = '/usr/local/google/home/manukh/personal/colorChrome/translateRunColors.js';
+
+const argDescriptions = [
+	{
+		names: ['build', 'b'],
+		values: 0,
+		example: '-b',
+		explanation: 'rebuilds.'
+	}, {
+		names: ['channel', 'c'],
+		values: 1,
+		example: '-c beta',
+		explanation: 'sets fake-variations-channel=beta and variations-server-url=http://localhost:8080/seed'
+	}, {
+		names: ['dogfood', 'd'],
+		values: 0,
+		example: '-d',
+		explanation: 'sets variations-server-url=http://localhost:8080/seed?restrict=dogfood'
+	}, {
+		names: ['study', 's'],
+		defaultValues: ['OmniboxBundledExperimentV1'],
+		values: 1,
+		example: '-s OmniboxBundledExperimentV1',
+		explanation: 'overrides the default study "OmniboxBundledExperimentV1" used below'
+	}, {
+		names: ['group', 'g'],
+		values: 1,
+		example: '-g Dev_Desktop_OmniboxDocumentMinCharacters_Control_V2',
+		explanation: 'sets force-fieldtrials=study/group'
+	}, {
+		names: ['feature', 'f'],
+		values: 2,
+		example: '-f OmniboxLocalEntitySuggestions ...',
+		explanation: 'sets enable-features="OmniboxLocalEntitySuggestions,...<study'
+	}, {
+		names: ['param', 'p'],
+		values: 2,
+		example: '-p UIMaxAutocompleteMatches 8 OmniboxMaxURLMatches 4 ...',
+		explanation: 'sets force-fieldtrial-params=study.group:UIMaxAutocompleteMatches/8/OmniboxMaxURLMatches/4/...'
+	}, {
+		names: ['gwsid', 'gi'],
+		values: 2,
+		example: '-gi t3315884 3314219 ...',
+		explanation: 'sets force-variation-ids="t3315884,3314219,..."'
+	}, {
+		names: ['google', 'gbu'],
+		values: 1,
+		example: '-gbu https://manukh-4.demos.corp.google.com/',
+		explanation: 'sets google-base-url=https://manukh-4.demos.corp.google.com/'
+	},
+];
+
+ArgsListParser.bashColorConsole();
+let args = new ArgsListParser(argDescriptions).parse();
+if (!args)
+	return;
+
+// Create command
+let commandArgs = [CHROME_COMMAND];
+if (args.channel)
+	commandArgs.push(`fake-variations-channel=${args.channel[0]}`);
+if (args.dogfood)
+	commandArgs.push('variations-server-url=http://localhost:8080/seed?restrict=dogfood');
+else if (args.channel)
+	commandArgs.push('variations-server-url=http://localhost:8080/seed');
+if (args.group)
+	commandArgs.push(`force-fieldtrials=${args.study[0]}/${args.group[0]}`);
+if (args.feature)
+	commandArgs.push(`enable-features='${args.feature.map(feature => `${feature}<${args.study[0]}`).join(',')}'`);
+if (args.param)
+	commandArgs.push(`force-fieldtrial-params=${args.study[0]}.${(args.group || [''])[0]}:${args.param.join('/')}`);
+if (args.gwsid)
+	commandArgs.push(`force-variation-ids='${args.gwsid.join(',')}'`);
+if (args.google)
+	commandArgs.push(`google-base-url=${args.google[0]}`);
+
+let chromeCommand = commandArgs.join('\\\n  --');
+console.white(`\n${chromeCommand}\n`);
+
+let pipedCommand = (command, outputPipe) => `unbuffer ${command} | node ${outputPipe} &&`;
+let pipedBuildCommand = args.build ? pipedCommand(BUILD_COMMAND, BUILD_OUTPUT_PIPE) : '';
+let pipedChromeCommand = pipedCommand(chromeCommand, CHROME_OUTPUT_PIPE);
+let command = `set -o pipefail && pushd ${CHROME_DIR} && ${pipedBuildCommand} ${pipedChromeCommand} popd`;
+
+let spawnedData = data => data.toString().trim();
+let spawned = childProcess.spawn(command, {shell: true});
+spawned.stdout.on('data', data => console.log(spawnedData(data)));
+spawned.stderr.on('data', data => console.error(spawnedData(data)));
+spawned.on('error', err => console.error('err', err));
---
+++ readme.md
@@ -0,0 +1,65 @@
+# cfinch
+
+## Description
+
+Generate command line arguments for running chromium with finch configurations.
+
+## Example
+
+```
+cfinch \
+  -g x \
+  -f OmniboxMaxZeroSuggestMatches \
+     OmniboxOnFocusSuggestions \
+   -p $(encode "ZeroSuggestVariant:*:*") $(encode "RemoteNoUrl,Local") \
+      MaxZeroSuggestMatches 3
+```
+
+Will generate
+
+```
+out/Default/chrome\
+  --fake-variations-channel=stable\
+  --variations-server-url=http://localhost:8080/seed\
+  --force-fieldtrials=OmniboxBundledExperimentV1/x\
+  --enable-features='OmniboxMaxZeroSuggestMatches<OmniboxBundledExperimentV1,OmniboxOnFocusSuggestions<OmniboxBundledExperimentV1'\
+  --force-fieldtrial-params=OmniboxBundledExperimentV1.x:ZeroSuggestVariant%3A*%3A*/RemoteNoUrl%2CLocal/MaxZeroSuggestMatches/3
+```
+
+## Params supported
+
+1. `fake-variations-channel`
+
+1. `variations-server-url`
+
+1. `force-fieldtrials`
+
+1. `enable-features`
+
+1. `force-fieldtrial`
+
+1. `force-variation-ids`
+
+1. `google-base-url`
+
+## Usage 
+
+1. clone this repo & run `npm i`
+
+1. Add an alias `cfinch` to `yourPath/chromeFinch/chromeFinch.js "$@"`
+
+1. Run `cfinch help` to print:
+
+```
+> cfinch help
+Arguments:
+    -b                                                          build|b       no values           rebuilds.                                                                                     
+    -c beta                                                     channel|c     single string       sets fake-variations-channel=beta and variations-server-url=http://localhost:8080/seed        
+    -d                                                          dogfood|d     no values           sets variations-server-url=http://localhost:8080/seed?restrict=dogfood                        
+    -s OmniboxBundledExperimentV1                               study|s       single string       overrides the default study "OmniboxBundledExperimentV1" used below                           
+    -g Dev_Desktop_OmniboxDocumentMinCharacters_Control_V2      group|g       single string       sets force-fieldtrials=study/group                                                            
+    -f OmniboxLocalEntitySuggestions ...                        feature|f     multiple strings    sets enable-features="OmniboxLocalEntitySuggestions,...<study                                 
+    -p UIMaxAutocompleteMatches 8 OmniboxMaxURLMatches 4 ...    param|p       multiple strings    sets force-fieldtrial-params=study.group:UIMaxAutocompleteMatches/8/OmniboxMaxURLMatches/4/...
+    -gi t3315884 3314219 ...                                    gwsid|gi      multiple strings    sets force-variation-ids="t3315884,3314219,..."                                               
+    -gbu https://manukh-4.demos.corp.google.com/                google|gbu    single string       sets google-base-url=https://manukh-4.demos.corp.google.com/                    
+```
