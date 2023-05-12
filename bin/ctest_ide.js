06850bd58fc209c203e87fd25b204cea6ea9b641
mahhov1 <mahhov1@gmail.com>
2023-05-11 19:12:00 -0700

    test2

--- bin/ctest_ide.js
+++ bin/ctest_ide.js
@@ -4,56 +4,91 @@ const path = require('path');
 const fs = require('fs').promises;
 const ChromeTest = require('../src/ChromeTest');
 
+const cachePath = path.resolve(__dirname, '_cached_ctest_ide_inputs.txt');
+
+let readCachedTestFilePathAndFilterName = async () => {
+	let read = (await fs.readFile(cachePath).catch(() => '')).toString()?.split('\n');
+  if (read?.length === 2)
+    return read;
+  console.warn('Missing testFilePath.');
+	return ['', '*'];
+};
+
+let writeCachedTestFilePathAndFilterName = (testFilePath, filterName) =>
+	fs.writeFile(cachePath, [testFilePath, filterName].join('\n'));
+
+let getTestSet = testFilePath => {
+  if (testFilePath.includes('/chromium/src/components/'))
+    return 'components_unittests';
+  if (testFilePath.includes('/chromium/src/ui/views/'))
+    return 'views_unittests';
+  if (testFilePath.includes('browsertest.cc'))
+    return 'browser_tests';
+  return 'unit_tests';
+};
+
+let getFilterName = async testFilePath => {
+  let fileContent = (await fs.readFile(testFilePath)).toString();
+  let filterName = [...fileContent.matchAll(
+      /TEST(?:_F|_P|)\(\s*(\w+),/g)]
+      .map(m => m[1])
+      .filter((v, i, a) => a.indexOf(v) === i)
+      .map(testName => `*${testName}.*`)
+      .join(':');
+  if (filterName)
+    return filterName;
+  console.warn('Missing filterName.');
+  return '*';
+}
+
 let asyncWrapper = async () => {
 	// E.g. out/Default
 	let out = process.argv[2];
 	// E.g. <...>/src/components/omnibox/browser/document_provider_unittest.cc
 	let testFilePath = process.argv[3];
-	// E.g. AutocompleteResultTest
-	let filterName = process.argv[4];
+	// E.g. AutocompleteResultTest, 0, or undfeined
+	let filterSeed = process.argv[4];
 
-	let cachePath = path.resolve(__dirname, '_cached_ctest_ide_inputs.txt');
-	if (testFilePath && testFilePath.match(/test\.cc$/))
-		fs.writeFile(cachePath, testFilePath);
-	else {
-		testFilePath = (await fs.readFile(cachePath).catch(() => '')).toString();
-		if (!testFilePath)
-			return console.warn('Missing testFilePath.');
-	}
+  // if inTestFile
+  //  and filterSeed = 0         -> autogenerate
+  //  and filterSeed = undefined -> read cached
+  //  and filterSeed set         -> use
 
-	let testSet;
-	if (testFilePath.includes('/chromium/src/components/'))
-		testSet = 'components_unittests';
-	else if (testFilePath.includes('/chromium/src/ui/views/'))
-		testSet = 'views_unittests';
-	else if (testFilePath.includes('browsertest.cc'))
-		testSet = 'browser_tests';
-	else
-		testSet = 'unit_tests';
+  // if !inTestFile
+  //  and filterSeed = 0              -> autogenerate
+  //  and filterSeed undefined or set -> read cached
 
-	let testFileContent = (await fs.readFile(testFilePath)).toString();
-	let testName = filterName ?
-      `*${filterName}*`
-      :[...testFileContent.matchAll(/TEST(?:_F|_P|)\(\s*(\w+),/g)]
-		.map(m => m[1])
-		.filter((v, i, a) => a.indexOf(v) === i)
-		.map(testName => `*${testName}.*`)
-		.join(':');
-	if (!testName)
-		return console.warn('Missing testName.');
+  let isTestFile = testFilePath.match(/test\.cc$/);
+  // E.g., *AutocompleteResultTest.*
+  let filterName;
+	if (!isTestFile)
+    [testFilePath, filterName] = await readCachedTestFilePathAndFilterName();
+  else if (!filterSeed)
+    filterName = (await readCachedTestFilePathAndFilterName())[1];
+
+  if (filterSeed === '0')
+    filterName = await getFilterName(testFilePath);
+  else if (isTestFile && filterSeed)
+    filterName = `*${filterSeed}*`;
+
+  if (isTestFile || filterSeed === '0')
+    writeCachedTestFilePathAndFilterName(testFilePath, filterName);
 
 	new ChromeTest().run([
 		'-b',
 		'-o', out,
-		'-s', testSet,
-		'-f', testName,
+		'-s', getTestSet(testFilePath),
+		'-f', filterName,
 	]);
 };
 
 asyncWrapper();
 
 // Translates
-//   ctest_ide out/Default <...>/src/components/omnibox/browser/document_provider_unittest.cc
+//   ctest_ide out/Default <...>/src/components/omnibox/browser/document_provider_unittest.cc 0
 // to
 //   out/Default/components_unittests --gtest_filter=DocumentProviderTest.*
 
+// 1st arg is out dir.
+// 2nd arg is test file path. If not a test file, read from cache.
+// 3rd arg is optional filgetName. If omitted, read from cache. If '0', autogenerates from file.
